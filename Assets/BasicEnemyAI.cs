4a7c524305d5be42d98fb799351e10bb87e2e65a
Jared <jaredvig@gmail.com>
2023-02-09 23:39:41 -0800

    fixed bug with enemy rotating when stuck, also made the enemy jump if the player was in view and at a higher elevation than the enemy

--- Assets/BasicEnemyAI.cs
+++ Assets/BasicEnemyAI.cs
@@ -6,10 +6,12 @@ public class BasicEnemyAI : MonoBehaviour
 {
     FieldOfView fov;
     CharacterController controller;
-    public float walkSpeed = 2;
+    public float walkSpeed = 2.5f;
     public Movement movementHelper;
     public Vector3 velocity;
     public float gravity = -9.807f;
+    public float jumpHeight = 1;
+    public Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,7 @@ public class BasicEnemyAI : MonoBehaviour
         this.controller = GetComponent<CharacterController>();
         this.movementHelper = GetComponent<Movement>();
         this.fov = GetComponent<FieldOfView>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -25,8 +28,13 @@ public class BasicEnemyAI : MonoBehaviour
         if (controller.isGrounded && velocity.y < 0) velocity.y = 0f;
         Vector3 movement = new Vector3(0, 0, 0);
         if (fov.canSeePlayer) {
-            this.transform.LookAt(fov.playerReference.transform);
+            Vector3 targetFlatPosition = new Vector3(fov.playerReference.transform.position.x, this.transform.position.y, fov.playerReference.transform.position.z);
+            this.transform.LookAt(targetFlatPosition);
             movement+=movementHelper.ForwardBack(walkSpeed, 1);
+            if (controller.isGrounded&&this.transform.position.y+0.5< fov.playerReference.transform.position.y) {
+                this.velocity.y = movementHelper.Jump(jumpHeight, gravity);
+            }
+            
         }
         this.velocity.y += this.gravity * Time.deltaTime;
         controller.Move((movement + this.velocity) * Time.deltaTime);
