fab0a9a20a0907a442008e4cd7f52ccdeeb38729
Jared Vigliecca <jaredvig@gmail.com>
2023-02-17 01:02:10 -0800

    added a health system, and an overlay that allows you to see the players health as it decreases

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     // Start is called before the first frame update
     public int health;
     public int maxHealth;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
     void Start()
     {
         health = maxHealth;
@@ -15,6 +19,24 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (hearts != null) {
+            for (int i = 0; i < hearts.Length; i++) {
+                if (i < health) {
+                    hearts[i].sprite = fullHeart;
+                }
+                else
+                {
+                    hearts[i].sprite = emptyHeart;
+                }
+                if (i < maxHealth) {
+                    hearts[i].enabled = true;
+                }
+                else
+                {
+                    hearts[i].enabled = false;
+                }
+            }
+        }
         if (health <= 0) {
             Destroy(this.gameObject);
         }
