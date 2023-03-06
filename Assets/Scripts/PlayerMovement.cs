5d6251d03365e54cb9a7b71d73a75036db5756ce
Tristan Gaeta <tristangaeta@gmail.com>
2023-03-05 17:31:25 -0800

    Combat Friendly User Input.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -30,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
     private float stepOffset;
     private bool isJumping;
     private bool isGrounded;
+    private float maxSpeed = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -39,41 +40,56 @@ public class PlayerMovement : MonoBehaviour
         this.stepOffset = controller.stepOffset;
     }
 
+    /**
+    * Returns the users input movement direction as a normalized 
+    * vector, and outputs the original magnitude clamped [0,1]
+    */
+    Vector3 GetMovementInput(out float magnitude)
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 input = new Vector3(horizontal, 0, vertical);
+        // get clamped magnitude and allow keyboard walk
+        magnitude = Mathf.Clamp01(input.magnitude);
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) magnitude *= 0.5f;
+        // normalize movement direction
+        input.Normalize();
+        return input;
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (knockbackTime <= 0)
         {
-            // stop accumulation of gravity
-            if (this.controller.isGrounded && this.yVelocity < 0f) this.yVelocity = 0f;
-            // running
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
-            float magnitude;
+            /* Movement Input */
+            float inputMagnitude;
+            Vector3 inputDirection = this.GetMovementInput(out inputMagnitude);
 
-            Vector3 direction = Movement.GetDirectionWASD(horizontal, vertical, this.cameraTransform.rotation.eulerAngles.y, out magnitude);
-            //walking
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) magnitude *= 0.5f;
-            this.animator.SetFloat("Input Magnitude", magnitude, 0.05f, Time.deltaTime);
+            float targetSpeed = inputMagnitude * this.maxSpeed;
+            this.animator.SetFloat("X Velocity", inputDirection.x * targetSpeed, 0.05f, Time.deltaTime);
+            this.animator.SetFloat("Z Velocity", inputDirection.z * targetSpeed, 0.05f, Time.deltaTime);
 
-            //  *** Jumping ***
-            if (this.controller.isGrounded) this.lastGroundedTime = Time.time;
+            /* Jumping */
+            if (this.controller.isGrounded)
+            {
+                this.lastGroundedTime = Time.time;
+                // stop accumulation of gravity
+                if (this.yVelocity < 0f) this.yVelocity = 0f;
+            }
             if (Input.GetButtonDown("Jump")) this.lastJumpRequestTime = Time.time;
 
             // player is on the ground (within grace period)
             if (Time.time - this.lastGroundedTime <= this.jumpGracePeriod)
             {
                 this.controller.stepOffset = this.stepOffset;
-                this.animator.SetBool("Is Grounded", true);
                 this.isGrounded = true;
-                this.animator.SetBool("Is Jumping", false);
                 this.isJumping = false;
                 this.animator.SetBool("Is Falling", false);
 
                 if (Time.time - this.lastJumpRequestTime <= this.jumpGracePeriod)
                 {
                     this.yVelocity = Movement.Jump(jumpHeight, Physics.gravity.y);
-                    this.animator.SetBool("Is Jumping", true);
                     this.isJumping = true;
                     this.lastJumpRequestTime = null;
                     this.lastGroundedTime = null;
@@ -82,7 +98,6 @@ public class PlayerMovement : MonoBehaviour
             else
             {
                 this.controller.stepOffset = 0;
-                this.animator.SetBool("Is Grounded", false);
                 this.isGrounded = false;
 
                 if ((this.isJumping && this.yVelocity <= 0) || this.yVelocity < this.fallThreshold)
@@ -91,24 +106,21 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
 
+            // update animator params
+            this.animator.SetBool("Is Jumping", this.isJumping);
+            this.animator.SetBool("Is Grounded", this.isGrounded);
+            this.animator.SetBool("Is Moving", inputDirection != Vector3.zero);
+
             // add gravity
             this.yVelocity += Physics.gravity.y * Time.deltaTime;
 
             // update rotation
-            if (direction != Vector3.zero)
-            {
-                this.animator.SetBool("Is Moving", true);
-                Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-                this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, targetRotation, this.turnSpeed * Time.deltaTime);
-            }
-            else
-            {
-                this.animator.SetBool("Is Moving", false);
+            Quaternion cameraRotation = Quaternion.AngleAxis(this.cameraTransform.eulerAngles.y, Vector3.up);
+            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, cameraRotation, this.turnSpeed * Time.deltaTime);
 
-            }
             if (this.isGrounded == false)
             {
-                Vector3 velocity = direction * (magnitude * this.jumpHorizontalSpeed);
+                Vector3 velocity = (cameraRotation * inputDirection) * (inputMagnitude * this.jumpHorizontalSpeed);
                 velocity.y = this.yVelocity;
                 this.controller.Move(velocity * Time.deltaTime);
             }
